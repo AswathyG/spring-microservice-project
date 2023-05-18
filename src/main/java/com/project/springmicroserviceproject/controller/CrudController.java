c12699824b0b9a4ae3f89dc227e5d7fac6a6a77d
AswathyG <aswathyg007@gmail.com>
2023-05-18 16:55:20 +0200

    http responses corrected

--- src/main/java/com/project/springmicroserviceproject/controller/CrudController.java
+++ src/main/java/com/project/springmicroserviceproject/controller/CrudController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,7 +39,7 @@ public class CrudController {
         CrudModel addedData = springProjectRepository.save(data);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                                      .buildAndExpand(addedData.getId()).toUri();
-        return ResponseEntity.created(location).build();                             
+        return ResponseEntity.created(location).headers(createHeaders()).body(addedData);                             
         
     }
 
@@ -51,24 +53,44 @@ public class CrudController {
         CrudModel data = retrievedData.get();
         data.setItemQuantity(data.getItemQuantity()+ quantityToBeAdded);
         springProjectRepository.save(data);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().headers(createHeaders()).body(retrievedData.get());
 
     }
    
     @GetMapping("/project/retrieve/{id}")
-    public CrudModel getItemDetail(@PathVariable int id){
+    public ResponseEntity<Object> getItemDetail(@PathVariable int id){
 
        Optional<CrudModel> retrievedData = springProjectRepository.findById(id);
        if(retrievedData.isEmpty()){
-        throw new EntryMissingException(" No data available for id : "+id);
+       // throw new EntryMissingException(" No data available for id : "+id);
+       return ResponseEntity.noContent()
+                            .header("Content-Type","application/json").build() ;
+                           
        } 
-       return retrievedData.get();
+                     
+       return new  ResponseEntity<Object>(retrievedData.get(),createHeaders(),HttpStatus.OK);
     }
 
     @DeleteMapping("/project/delete/{id}")
-    public String deleteItem(@PathVariable int id){
-        springProjectRepository.deleteById(id);
-        return "Successfully deleted";
-        
+    public ResponseEntity<String> deleteItem(@PathVariable int id){
+        try {
+            springProjectRepository.deleteById(id);
+
+            
+            return new ResponseEntity<String>("Entry deleted successfully!", createHeaders(), HttpStatus.OK);
+
+        } catch (Exception e) {
+            return ResponseEntity.noContent()
+                            .header("Content-Type","application/json").build() ;
+        }
+   
+    }
+
+    
+    public HttpHeaders createHeaders(){
+
+        HttpHeaders respHeader = new HttpHeaders();
+        respHeader.set("Content-Type", "application/json");   
+        return respHeader;
     }
 }
