b513a64128b80d4a391944e3eaa9e1d3ee7c56b8
AswathyG <aswathyg007@gmail.com>
2023-06-11 16:25:29 +0200

    Adding conversion to https related changes

--- src/main/java/com/project/springmicroserviceproject/SpringMicroserviceProjectApplication.java
+++ src/main/java/com/project/springmicroserviceproject/SpringMicroserviceProjectApplication.java
@@ -1,7 +1,12 @@
 package com.project.springmicroserviceproject;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringMicroserviceProjectApplication {
@@ -10,4 +15,18 @@ public class SpringMicroserviceProjectApplication {
 		SpringApplication.run(SpringMicroserviceProjectApplication.class, args);
 	}
 
+	@Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> 
+							webServerFactoryCustomizer(@Value("${server.port}") int serverPort) {
+        return factory -> {
+            if (factory instanceof TomcatServletWebServerFactory) {
+                TomcatServletWebServerFactory tomcatFactory = (TomcatServletWebServerFactory) factory;
+                tomcatFactory.addConnectorCustomizers(connector -> {
+                    connector.setScheme("https");
+                    connector.setSecure(true);
+                    connector.setPort(serverPort);
+                });
+            }
+        };
+    }
 }
